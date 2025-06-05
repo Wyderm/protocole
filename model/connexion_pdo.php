f286d453092c7ca9e7c8d16c6be21c9cebc64603
deghaye.m <matheo.deghaye@gmail.com>
2025-06-05 08:22:28 +0200

    chemin bdd

--- model/connexion_pdo.php
+++ model/connexion_pdo.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $chemin_base = '\\\\sv-fichiers\\Partages\\PO-Accompagner-SVC-CCAS\\Matheo Deghaye\\bdd_protocole\\bdd_protocole.db';
+    $chemin_base = '\\\\sv-ccas\\Data\\DEGHAYE\\protocole\\bdd_protocole.db';
     $db = new PDO("sqlite:$chemin_base");
 } catch (PDOException $e) {
     die ("Connection failed: " . $e->getMessage());
